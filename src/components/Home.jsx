8c415b4684dfa1eebdc737caa10898fc2115a086
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-07-27 23:56:20 +0500

    'aaj'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import background from '../assets/background.png'
+import image from '../assets/home.png'
+import {RiGroupLine} from 'react-icons/ri'
+import styled from 'styled-components'
+import Button from './Button'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Section id="home">
+    <div className='text'>
+      <h1>
+        <span className='highlight'>Earthium</span> is a real-time strategic game
+      </h1>
+      <p>
+        Where you earn money, <span className='highlight'>Crypto</span> and
+        <span className='highlight'>NFT's</span> by trading land, finding treasures and building businessess. Welcome!
+      </p>
+      <Button
+       text="Join our Discord"
+       icon={<RiGroupLine/>}
+      />
+    </div>
+
+    <div className='image'>
+      <img src={image} alt='Home Image' />
+    </div>
+    </Section>
   )
 }
 
 export default Home
+
+const Section = styled.section`
+  background-image: url(${background});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  display: grid;
+  ${'' /* grid-template-columns: 1fr 1fr; */}
+  grid-template-columns: 50% 50%;
+  height: 80vh;
+  align-items: center;
+
+  .image{
+    img{
+      width: 80%;
+    }
+  }
+`
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
-import StyledComponent  from 'styled-components'
 import logo from '../assets/logo.png'
 import flag from '../assets/flag.png'
 
 import {FiLock} from 'react-icons/fi'
 import {BsToggleOn, BsToggleOff} from 'react-icons/bs'
+import styled from 'styled-components'
 
 
 const Navbar = () => {
@@ -14,6 +14,7 @@ const Navbar = () => {
   return (
     <Nav>
         <div className='brand'>
+        <div className='logo'>
             <img src={logo} alt='brand logo' />
         </div>
         <div className='toggle'>
@@ -23,10 +24,136 @@ const Navbar = () => {
                 <BsToggleOff onClick={() => setNavState(true)} />
             }
         </div>
+        </div>
+
+       
+
+        <div className={`links ${navState ? "show" : "hide"}`}>
+
+            <ul>
+                <li>
+                    <a href='#home'>Home</a>
+                </li>
+                <li>
+                    <a href='#home'>Map</a>
+                </li>
+                <li>
+                    <a href='#home'>Contact</a>
+                </li>
+                <li>
+                    <a href='#home'>News</a>
+                </li>
+            </ul>
+
+            <div className='login-btn'>
+                <div className='flag'>
+                    <img src={flag} alt='flag' />
+                    <span>English</span>
+                </div>
+                <button>
+                    <FiLock /> Login & Registration
+                </button>
+            </div>
+
+        </div>
     </Nav>
   )
 }
 
 export default Navbar;
 
-const Nav = StyledComponent.nav``;
+const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    margin: 3rem 6rem;
+
+    .brand{
+        display: flex;
+        align-items: center;
+        
+        .logo{
+            img{
+                height: 5vh;
+            }
+        }
+
+        .toggle{
+            display: none;
+        }
+    }
+
+    .links{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 60%;
+
+        ul{
+            display: flex;
+            list-style-type: none;
+            gap: 2rem;
+
+            li{
+                padding: 0.5rem 1rem;
+                trasition: 0.3s ease-in-out;
+                border-radius: 0.3rem;
+
+                &:first-of-type,
+                &:hover
+                {
+                    background-color: var(--primary-color);
+                    a{
+                        color: #000;
+                       
+                    }
+                }
+
+                a{
+                    text-decoration: none;
+                    color: white;
+                    font-weight: bold;
+                    transition: 0.3s ease-in-out;
+                    font-family: var(--font-family)
+
+                }
+            } 
+        }
+
+        .login-btn{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            gap: 2rem;
+
+            .flag{
+                display: flex;
+                justify-content: center;
+                gap: 0.3rem;
+                align-items: center;
+                font-weight: bold;
+                cursor: pointer;
+            }
+
+            button{
+                border: 1px solid var(--primary-color);
+                background-color: transparent;
+                color: #fff;
+                border-radius: 0.3rem;
+                padding: 0.8rem;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                gap: 0.7rem;
+                font-weight: bold;
+                cursor: pointer;
+                
+                svg{
+                    font-size: 1.1rem;
+                    font-weight: bold;
+
+                }
+            }
+        }
+    }
+
+`;
