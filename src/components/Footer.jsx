116e8761c5adadc4f4f8bfcf0ece5d60e36a5ca2
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-07-30 00:39:49 +0500

    aajcreative

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 import logo from '../assets/logo.png'
+import Button from './Button'
 
 const Footer = () => {
 
@@ -32,12 +33,41 @@ const Footer = () => {
     <Foot>
     <div className='upper_footer'>
       <div className='brand'>
-        <img src={logo} alt='brand logo' />
+        <img src={logo} alt='footer logo' />
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+        </p>
+      <div className='mail-container'>
+        <input type='email' placeholder='Enter Email' />
+        <Button text="Get Started" />
       </div>
+      </div>
+      {
+        links.map(({title, links}) => {
+          return(
+            <div className='links' key="title">
+            <h3>{title}</h3>
+            <ul>
+              {
+                links.map((link) => {
+                  <li key={links}> {link} </li>
+                })
+              }
+            </ul>
+            </div>
+          )
+        })
+      }
     </div>
 
     <div className='lower_footer'>
-
+      <span> 2022 Earthium </span>
+      <ul>
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>LinkedIn</li>
+        <li>Instagram</li>
+      </ul>
     </div>
 
     </Foot>
@@ -46,4 +76,72 @@ const Footer = () => {
 
 export default Footer
 
-const Foot = styled.footer``
+const Foot = styled.footer`
+  border-top: 0.02rem solid var(--primary-color);
+  margin: 5rem 0 1rem 0;
+  .upper_footer{
+    margin: 0 5rem;
+    display: grid;
+    grid-template-columns: 3fr 1fr 1fr 1fr;
+    gap: 3rem;
+    padding-top: 3rem;
+    .brand{
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 1rem;
+      img{
+        height: 2rem;
+      }
+      .mail-container{
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+
+        input{
+          padding: 0.8rem 1rem;
+          border-radius: 0.3rem;
+          border: none;
+          &:focus{
+            outline: none;
+          }
+        }
+
+        button{
+          padding: 0.8rem 1rem;
+          gap: 0.5rem;
+        }
+      }
+    }
+
+    .links{
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+
+      h3{
+        color: var(--primary-color);
+      }
+      ul{
+        display: flex;
+        flex-direction: column;
+        gap: 0.8rem;
+        li{
+          cursor: pointer;
+          transition: 0.3s ease-in-out;
+          &:hover{
+            color: var(--primary-color);
+          }
+        }
+      }
+    }
+  }
+
+
+  .lower_footer{
+    margin: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+    border-top: 0.2rem solid var(--primary-color);
+  }
+`
